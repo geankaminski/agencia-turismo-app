ca0295c72e1d11eb439b1bffd901693f82263079
Gean Kaminski <geankaminski93@hotmail.com>
2021-03-13 09:05:13 -0300

    update view

--- app/src/main/java/br/com/alura/aluraviagens/ui/adapter/ListaPacotesAdapter.java
+++ app/src/main/java/br/com/alura/aluraviagens/ui/adapter/ListaPacotesAdapter.java
@@ -1,12 +1,20 @@
 package br.com.alura.aluraviagens.ui.adapter;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.List;
+import java.util.Locale;
 
 import br.com.alura.aluraviagens.R;
 import br.com.alura.aluraviagens.model.Pacote;
@@ -40,6 +48,33 @@ public class ListaPacotesAdapter extends BaseAdapter {
     @Override
     public View getView(int posicao, View convertView, ViewGroup parent) {
         View viewCriada = LayoutInflater.from(context).inflate(R.layout.item_pacote, parent, false);
+
+        Pacote pacote = pacotes.get(posicao);
+
+        TextView local = viewCriada.findViewById(R.id.item_pacote_local);
+        local.setText(pacote.getLocal());
+
+        ImageView imagem = viewCriada.findViewById(R.id.item_pacote_imagem);
+        Resources resources = context.getResources();
+        int idDoDrawable = resources.getIdentifier(pacote.getImagem(), "drawable", context.getPackageName());
+        Drawable drawableImagemPacote = resources.getDrawable(idDoDrawable);
+        imagem.setImageDrawable(drawableImagemPacote);
+
+        TextView dias = viewCriada.findViewById(R.id.item_pacote_dias);
+        int quantidadeDeDias = pacote.getDias();
+        String dia = " dia";
+        if(quantidadeDeDias > 1) {
+            dia = " dias";
+        }
+        dias.setText(pacote.getDias() + dia);
+
+        TextView preco = viewCriada.findViewById(R.id.item_pacote_preco);
+        BigDecimal precoDoPacote = pacote.getPreco();
+        NumberFormat formatoBrasileiro = DecimalFormat.getCurrencyInstance(new Locale("pt", "br"));
+        String moedaBrasileira = formatoBrasileiro.format(precoDoPacote).format("R$", "R$ ");
+        preco.setText(moedaBrasileira);
+
+
         return viewCriada;
     }
 }
